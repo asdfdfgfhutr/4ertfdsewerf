4ef9757b16bafbbf990fec70ee549ddb2de4adfb
asdfdfgfhutr <127807462+asdfdfgfhutr@users.noreply.github.com>
2023-03-14 05:15:51 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=f0834215-694f-491a-b307-c0bcc949f1e8
+export UUID=e4f98bf9-a672-4ef9-af43-4b7f405e2778
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=netsrepublic.com
+export ProxySite=arsenalfclatestnews.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
