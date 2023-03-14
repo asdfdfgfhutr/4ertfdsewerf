711b5f89cb5df8c3788706db4c9cd1f708056986
asdfdfgfhutr <127807462+asdfdfgfhutr@users.noreply.github.com>
2023-03-14 01:58:17 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=f0834215-694f-491a-b307-c0bcc949f1e8
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=netsrepublic.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
